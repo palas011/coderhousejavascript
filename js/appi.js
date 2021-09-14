4df5af93ac95720fd1b55f54673a14e41b93f981
palas011 <nicolaspalacios@hotmail.com>
2021-09-14 15:28:33 -0300

    desafio n1 nicolas palacios

---
+++ js/appi.js
@@ -0,0 +1,7 @@
+let nombreAlumno = prompt("Ingrese el nombre del alumno");
+let edadAlumno   = prompt("Ingrese la edad del alumno");
+let ciudadAlumno = prompt("Ingrese la ciudad nombre del alumno");
+
+let datosAlumno = "el almumno" + " " + nombreAlumno + " " +  "tiene" + " " + edadAlumno  + " " + "y vive en" + " "  + ciudadAlumno;
+
+alert(datosAlumno);
