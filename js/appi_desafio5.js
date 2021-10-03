dcf7d2fd274c212c6bb506a354a3491c752019e1
palas011 <nicolaspalacios@hotmail.com>
2021-10-03 14:11:45 -0300

    dasafio clase 5

--- js/appi_desafio5.js
+++ js/appi_desafio5.js
@@ -20,11 +20,28 @@ class Credito {
 }
 
 const nuevaSolicitud = () => {
+    let sucursal = prompt("Ingresar Sucursal")
     let nombre =  prompt("Ingrese su nombre")
     let sueldo =  prompt("Ingrese su sueldo neto")
     let credito = prompt("Cuanto dinero necesita?")
     let cuotas = prompt("En cuantas cuotas? (12-15-18)")
     const pedido = new Credito(nombre,sueldo,credito,cuotas)
     console.log(pedido);
+
+    //CREO ARRAY SOLO CON LA FECHA
+    const fecha = new Date();
+    const solicitudes = [fecha];
+
+    //AGREGO DATOS TECNICOS//
+    const datostecnicos ={sucursal,nombre,sueldo,fecha}
+    solicitudes.push([datostecnicos]);
+
+    //AGREGO DATOS DEL VENDEDOR
+    nombrevendedor = "Juan 01";
+    comision = credito * 0.02;
+    const datosvendedor ={nombrevendedor,comision}
+    solicitudes.push([datosvendedor]);
+    console.log(solicitudes);
+    
 }
 
